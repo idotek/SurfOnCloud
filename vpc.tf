7237c9d95e388397fc5684d7b8ca51ae0aa4a0c0
Tortele <lagneauthibaud@gmail.com>
2024-03-20 15:27:30 +0100

    Création du VPC Peering

--- vpc.tf
+++ vpc.tf
@@ -15,3 +15,21 @@ resource "aws_vpc" "vpc_internal" {
     Name = "VPC INTERNAL"
   }
 }
+#Création du VPC Peering
+resource "aws_vpc_peering_connection" "vpc_peer" {
+  peer_vpc_id   = aws_vpc.vpc_prod.id
+  vpc_id        = aws_vpc.vpc_internal.id
+  auto_accept = true
+
+  accepter {
+    allow_remote_vpc_dns_resolution = true
+  }
+
+  requester {
+    allow_remote_vpc_dns_resolution = true
+  }
+
+  tags = {
+    Name = "VPC Peering entre Internal et Prod"
+  }
+}
