e4437d7148c85032dd67ba3e64ee9db90bc7523e
Tortele <lagneauthibaud@gmail.com>
2024-03-20 15:29:40 +0100

    Résolution DNS retirée

--- vpc.tf
+++ vpc.tf
@@ -20,15 +20,7 @@ resource "aws_vpc_peering_connection" "vpc_peer" {
   peer_vpc_id   = aws_vpc.vpc_prod.id
   vpc_id        = aws_vpc.vpc_internal.id
   auto_accept = true
-
-  accepter {
-    allow_remote_vpc_dns_resolution = true
-  }
-
-  requester {
-    allow_remote_vpc_dns_resolution = true
-  }
-
+  
   tags = {
     Name = "VPC Peering entre Internal et Prod"
   }
