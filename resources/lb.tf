6774217569d32ec579b8bb412196b362c2593b01
mmorelle <mmorelle@mmr.corp.wallix.com>
2024-03-20 10:18:25 +0100

    New lb for preprod

--- resources/lb.tf
+++ resources/lb.tf
@@ -1,5 +1,5 @@
-resource "aws_elb" "TF-LB" {
-  name    = "TF-LB"
+resource "aws_elb" "LB-Prod" {
+  name    = "LB-Prod"
   subnets = [aws_subnet.Web-1.id, aws_subnet.Web-2.id, aws_subnet.Web-3.id]
   security_groups = [aws_security_group.LbNSG.id]
   listener {
@@ -24,7 +24,43 @@ resource "aws_elb" "TF-LB" {
   connection_draining_timeout = 400
 
   tags = {
-    Name = "TF-LB"
+    Name = "LB-Prod"
+  }
+}
+
+
+
+
+
+
+
+resource "aws_elb" "LB-Internal" {
+  name    = "LB-Internal"
+  subnets = [aws_subnet.WebDev-1.id, aws_subnet.WebDev-2.id, aws_subnet.Web-Dev3.id]
+  security_groups = [aws_security_group.LbNSG.id]
+  listener {
+    instance_port     = 80
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    target              = "HTTP:80/"
+    interval            = 30
+  }
+
+  instances                   = [aws_instance.WebDev-1.id, aws_instance.WebDev-2.id, aws_instance.WebDev-3.id]
+  cross_zone_load_balancing   = true
+  idle_timeout                = 400
+  connection_draining         = true
+  connection_draining_timeout = 400
+
+  tags = {
+    Name = "LB-Internal"
   }
 }
 
